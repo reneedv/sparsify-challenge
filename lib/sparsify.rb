4b28847b69027814067b3036188ca182726e8d7a
Renée De Voursney <renee@nird.us>
2013-10-21 15:30:18 -0700

    first pass, needs clean-up

--- lib/sparsify.rb
+++ lib/sparsify.rb
@@ -1,5 +1,27 @@
 # encoding: utf-8
 
 module Sparsify
-  # Your implementation goes here
+  def self.sparse source
+    result = {}
+    source.each do |k,v|
+      puts (flatten k,v)
+      result.merge!(flatten k,v)
+    end
+    result
+  end
+
+private
+
+  def self.flatten k,v
+    if v.respond_to? :each
+      res = {}
+      v.each do |ki, vi|
+        res.merge!(flatten "#{k}.#{ki}", vi)
+      end
+      res
+    else
+      {k => v}
+    end
+  end
+
 end
