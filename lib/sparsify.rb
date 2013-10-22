489989a36e397929d2cdc259286716ae61b5c8ab
Renée De Voursney <renee@nird.us>
2013-10-21 19:32:27 -0700

    simple step 2

--- lib/sparsify.rb
+++ lib/sparsify.rb
@@ -2,18 +2,47 @@
 
 module Sparsify
   def self.sparse source
+    sparsey source, :flatten
+  end
+
+  def self.unsparse source
     result = {}
-    source.each do |k,v|
-      puts (flatten k,v)
-      result.merge!(flatten k,v)
+    obj_keys = source.keys.select{|k| k.include?('.')}
+
+    top_level = obj_keys.map{|o| o.split('.').first}.uniq
+
+    top_level.each do |t|
+      result[t] = {}
     end
+
+    source.each do |k,v|
+      if k.count('.') > 1
+        keys = k.split('.')
+        one = keys.first
+        result[one][keys[1]] = {} if result[one][keys[1]].nil?
+        result[one][keys[1]].merge!( {keys[2] => v})
+      elsif k.count('.') == 1
+        result[k.split('.').first][k.split('.')[1]] = v
+      else
+        result.merge!({k => v})
+      end
+    end
+
     result
   end
 
 private
 
+  def self.sparsey source, method
+    result = {}
+    source.each do |k,v|
+      result.merge!(send(method,k,v))
+    end
+    result
+  end
+
   def self.flatten k,v
-    if v.respond_to? :each
+    if v.kind_of? Hash
       res = {}
       v.each do |ki, vi|
         res.merge!(flatten "#{k}.#{ki}", vi)
@@ -22,6 +51,10 @@ private
     else
       {k => v}
     end
+  end
+
+  def self.unflatten k,v,res
+
   end
 
 end
