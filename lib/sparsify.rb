d938c14d87c514523c3b95c0bbd4ad7b745dbd30
Renée De Voursney <renee@nird.us>
2013-10-21 20:22:09 -0700

    almost there

--- lib/sparsify.rb
+++ lib/sparsify.rb
@@ -1,28 +1,37 @@
 # encoding: utf-8
 
 module Sparsify
-  def self.sparse source
-    sparsey source, :flatten
+  def self.sparse source, h={separator: '.'}
+    separator = h[:separator]
+    result = {}
+    source.each do |k,v|
+      k = k.gsub(separator, "\\#{separator}")
+      result.merge!(flatten k,v,separator )
+    end
+    result
   end
 
-  def self.unsparse source
+  def self.unsparse source, h={separator: '.'}
+    separator = h[:separator]
     result = {}
-    obj_keys = source.keys.select{|k| k.include?('.')}
+    obj_keys = source.keys.select{|k| k.include?(separator)}
 
-    top_level = obj_keys.map{|o| o.split('.').first}.uniq
+    top_level = obj_keys.map{|o| o.split(separator).first}.uniq
 
     top_level.each do |t|
       result[t] = {}
     end
 
     source.each do |k,v|
-      if k.count('.') > 1
-        keys = k.split('.')
-        one = keys.first
-        result[one][keys[1]] = {} if result[one][keys[1]].nil?
-        result[one][keys[1]].merge!( {keys[2] => v})
-      elsif k.count('.') == 1
-        result[k.split('.').first][k.split('.')[1]] = v
+      if k.include?(separator) && !k.include?("\\#{separator}")
+        if k.count(separator) > 1
+          keys = k.split(separator)
+          one = keys.first
+          result[one][keys[1]] = {} if result[one][keys[1]].nil?
+          result[one][keys[1]].merge!( {keys[2] => v})
+        elsif k.count(separator) == 1
+          result[k.split(separator).first][k.split(separator)[1]] = v
+        end
       else
         result.merge!({k => v})
       end
@@ -33,28 +42,16 @@ module Sparsify
 
 private
 
-  def self.sparsey source, method
-    result = {}
-    source.each do |k,v|
-      result.merge!(send(method,k,v))
-    end
-    result
-  end
-
-  def self.flatten k,v
-    if v.kind_of? Hash
+  def self.flatten k, v, separator
+    if v.kind_of?(Hash) && !v.empty?
       res = {}
       v.each do |ki, vi|
-        res.merge!(flatten "#{k}.#{ki}", vi)
+        res.merge!(flatten "#{k}#{separator}#{ki}", vi, separator)
       end
       res
     else
       {k => v}
     end
-  end
-
-  def self.unflatten k,v,res
-
   end
 
 end
