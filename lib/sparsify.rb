969a46d5b1617aeb0db612c867a4c2069f19edee
Renée De Voursney <renee@nird.us>
2013-10-22 14:17:20 -0700

    almost and started refactor, but refactor isn't working yet

--- lib/sparsify.rb
+++ lib/sparsify.rb
@@ -13,30 +13,42 @@ module Sparsify
 
   def self.unsparse source, h={separator: '.'}
     separator = h[:separator]
+    #return unflatten source, separator
     result = {}
     obj_keys = source.keys.select{|k| k.include?(separator)}
 
-    top_level = obj_keys.map{|o| o.split(separator).first}.uniq
+    top_level = obj_keys.map{|o| o.split(/(?<!\\)#{Regexp.escape(separator)}/).first}.uniq.map{|k| k.gsub("\\#{separator}", separator)}
+
 
     top_level.each do |t|
       result[t] = {}
     end
 
     source.each do |k,v|
-      if k.include?(separator) && !k.include?("\\#{separator}")
+      if k.include?(separator)
         if k.count(separator) > 1
-          keys = k.split(separator)
+          keys = k.split(/(?<!\\)#{Regexp.escape(separator)}/).map{|k| k.gsub("\\#{separator}", separator)}
           one = keys.first
+          result[one] = {} if result[one].nil?
+          result[one][keys[1]] = v and next if keys.size == 2 
           result[one][keys[1]] = {} if result[one][keys[1]].nil?
           result[one][keys[1]].merge!( {keys[2] => v})
         elsif k.count(separator) == 1
           result[k.split(separator).first][k.split(separator)[1]] = v
         end
-      else
+      else 
         result.merge!({k => v})
       end
     end
 
+    keys = result.keys
+    values = result.values
+    result = {}
+    keys.each_with_index do |k,i|
+      v = values[i]
+      key = k.gsub("\\", "\\\\")
+      result[key] = v
+    end
     result
   end
 
@@ -53,5 +65,25 @@ private
       {k => v}
     end
   end
+
+  def self.unflatten source, separator
+    result = {}
+    keys = source.keys
+    values = source.values
+    keys.each_with_index do |k, i|
+      layer_keys = k.split(/(?<!\\)#{Regexp.escape(separator)}/)
+      layer_keys.reverse!
+      first = layer_keys.pop
+      result = {first => values[i]}
+      layer_keys.each do |key|
+        result[key] = {} if result[key].nil?
+        result[key].merge!(v)
+        v = {key => result[key]}
+      end
+    end
+    result
+  end
+
+  ## the key
 
 end
